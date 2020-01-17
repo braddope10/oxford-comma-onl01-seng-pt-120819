9a24c268262dc63163764fd4f64cbffc34aea04f
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-01-17 22:14:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,6 @@ def oxford_comma(array)
 end
   
 def oxford_comma(array)
-  array.size <= 2? array.join(" and ") : (array.first array.size -1).join(" , ")
+  array.size <= 2? array.join(" and ") : (array.first array.size -1).join(" , ") + ", and #{array.last}"
   
 end
